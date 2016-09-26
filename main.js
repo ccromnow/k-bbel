3dd55b598cbb1fd7c029aaf085f5c821d99de8a0
cromnow@kaustik.com <cromnow@kaustik.com>
2016-09-26 11:41:16 +0200

    changed discord settings

--- main.js
+++ main.js
@@ -1,23 +1,40 @@
-// create an instance of a Discord Client, and call it bot
-const bot = new Discord.Client();
-
 // the token of your bot - https://discordapp.com/developers/applications/me
 const token = 'MjI5ODc0NTcyMzY5OTg1NTM2.CsptOQ.6JAN2Y0YJLQM-07ffYAqPym54jU';
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
-// the ready event is vital, it means that your bot will only start reacting to information
-// from Discord _after_ ready is emitted.
-bot.on('ready', () => {
+client.on('ready', () => {
   console.log('I am ready!');
 });
 
-// create an event listener for messages
-bot.on('message', message => {
-  // if the message is "ping",
+client.on('message', message => {
   if (message.content === 'ping') {
-    // send "pong" to the same channel.
-    message.channel.sendMessage('pong');
+    message.reply('pong');
   }
 });
 
-// log our bot in
-bot.login(token);
+client.login(token);
+
+
+
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT || 8080;
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
+
+// set the home page route
+app.get('/', function(req, res) {
+
+	// ejs render automatically looks in the views folder
+	res.render('index');
+});
+
+app.listen(port, function() {
+	console.log('Our app is running on http://localhost:' + port);
+});
